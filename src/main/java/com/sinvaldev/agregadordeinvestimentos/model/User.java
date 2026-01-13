bc02684989d414fcd9eb14220c8ecfd02749c665
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-13 09:57:40 -0300

    feat: add user entity

---
+++ src/main/java/com/sinvaldev/agregadordeinvestimentos/model/User.java
@@ -0,0 +1,38 @@
+package com.sinvaldev.agregadordeinvestimentos.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "username", nullable = false)
+    private String userName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+}
