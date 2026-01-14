315c9bb7ec59a4f37c4ca6aa5c0a7c228a6f6f2a
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-14 10:05:19 -0300

    feat: creates the business rule for creating a user, integrating the service and the repository

---
+++ src/main/java/com/sinvaldev/agregadordeinvestimentos/service/UserService.java
@@ -0,0 +1,30 @@
+package com.sinvaldev.agregadordeinvestimentos.service;
+
+import com.sinvaldev.agregadordeinvestimentos.dtos.RequestUserDto;
+import com.sinvaldev.agregadordeinvestimentos.dtos.UserDto;
+import com.sinvaldev.agregadordeinvestimentos.exception.EmailAlreadyExistsException;
+import com.sinvaldev.agregadordeinvestimentos.mappers.UserMapper;
+import com.sinvaldev.agregadordeinvestimentos.model.User;
+import com.sinvaldev.agregadordeinvestimentos.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDto createUser(RequestUserDto requestUserDto) {
+        if (userRepository.existsUserByEmail(requestUserDto.email())) {
+            throw new EmailAlreadyExistsException("Email já cadastrado");
+        }
+
+        User user = userRepository.save(userMapper.requestUserDtoToUser(requestUserDto));
+        log.info("Usuário criado com sucesso");
+        return userMapper.userToUserDto(user);
+    }
+}
