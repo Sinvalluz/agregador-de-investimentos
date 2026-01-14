a34258d898ae701693e170b5fdc030a0d347ca9b
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-14 08:34:37 -0300

    feat: add a UserRepository interface that handles actions in the database

---
+++ src/main/java/com/sinvaldev/agregadordeinvestimentos/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sinvaldev.agregadordeinvestimentos.repository;
+
+import com.sinvaldev.agregadordeinvestimentos.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
