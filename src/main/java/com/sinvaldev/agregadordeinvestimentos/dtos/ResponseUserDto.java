d8cccb5b1b0df9ca42ca6ba9b491df9b361eddd4
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-15 09:28:50 -0300

    fix: the ResponseUserDto was also returning the user's password

--- src/main/java/com/sinvaldev/agregadordeinvestimentos/dtos/ResponseUserDto.java
+++ src/main/java/com/sinvaldev/agregadordeinvestimentos/dtos/ResponseUserDto.java
@@ -3,5 +3,5 @@ package com.sinvaldev.agregadordeinvestimentos.dtos;
 import java.time.Instant;
 import java.util.UUID;
 
-public record ResponseUserDto(UUID userId, String userName, String email, String password, Instant creationTimestamp, Instant updateTimestamp) {
+public record ResponseUserDto(UUID userId, String userName, String email , Instant creationTimestamp, Instant updateTimestamp) {
 }
