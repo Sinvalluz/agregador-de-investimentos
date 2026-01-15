1bf0640f37ba530841c2648a57f5084495f48919
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-15 09:05:04 -0300

    feat: adds the method for searching for users by ID

--- src/main/java/com/sinvaldev/agregadordeinvestimentos/service/UserService.java
+++ src/main/java/com/sinvaldev/agregadordeinvestimentos/service/UserService.java
@@ -3,11 +3,13 @@ package com.sinvaldev.agregadordeinvestimentos.service;
 import com.sinvaldev.agregadordeinvestimentos.dtos.RequestUserDto;
 import com.sinvaldev.agregadordeinvestimentos.dtos.UserDto;
 import com.sinvaldev.agregadordeinvestimentos.exception.EmailAlreadyExistsException;
+import com.sinvaldev.agregadordeinvestimentos.exception.UserNotFoundException;
 import com.sinvaldev.agregadordeinvestimentos.mappers.UserMapper;
 import com.sinvaldev.agregadordeinvestimentos.model.User;
 import com.sinvaldev.agregadordeinvestimentos.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -23,11 +25,18 @@ public class UserService {
 
     public UserDto createUser(RequestUserDto requestUserDto) {
         if (userRepository.existsUserByEmail(requestUserDto.email())) {
-            throw new EmailAlreadyExistsException("Email já cadastrado");
+            throw new EmailAlreadyExistsException();
         }
 
         User user = userRepository.save(userMapper.requestUserDtoToUser(requestUserDto));
         log.info("Usuário criado com sucesso");
+        return userMapper.userToUserDto(user);
+    }
+
+    public UserDto findUserById (String userId) {
+        User user = userRepository.findById(UUID.fromString(userId))
+                .orElseThrow(UserNotFoundException::new);;
+
         return userMapper.userToUserDto(user);
     }
 }
