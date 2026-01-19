fe6895d40b0c69b32a41997211f485f8205c987f
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-19 09:37:57 -0300

    test: add unit tests for the createUser method in UserService

---
+++ src/test/java/com/sinvaldev/agregadordeinvestimentos/service/UserServiceTest.java
@@ -0,0 +1,87 @@
+package com.sinvaldev.agregadordeinvestimentos.service;
+
+import com.sinvaldev.agregadordeinvestimentos.dtos.RequestUserDto;
+import com.sinvaldev.agregadordeinvestimentos.dtos.UserDto;
+import com.sinvaldev.agregadordeinvestimentos.exception.EmailAlreadyExistsException;
+import com.sinvaldev.agregadordeinvestimentos.mappers.UserMapper;
+import com.sinvaldev.agregadordeinvestimentos.model.User;
+import com.sinvaldev.agregadordeinvestimentos.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+// Arrange 👉 O que eu preciso preparar?
+// Act 👉 O que eu vou executar?
+// Assert 👉 O que eu espero que aconteça?
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Nested
+    class createUser {
+
+        @Test
+        @DisplayName("should create a user with successfully")
+        void shouldCreateAUser() {
+            // Arrange
+
+            RequestUserDto requestUserDto = new RequestUserDto("userName", "email@email.com", "password");
+            User user = new User(UUID.randomUUID(), "userName", "email@email.com", "password", Instant.now(), Instant.now());
+            UserDto userDto = new UserDto(user.getUserId(), user.getUserName(), user.getEmail(), user.getCreationTimestamp(), user.getUpdateTimestamp());
+
+            when(userRepository.existsUserByEmail(requestUserDto.email())).thenReturn(false);
+            when(userMapper.requestUserDtoToUser(requestUserDto)).thenReturn(user);
+            when(userRepository.save(user)).thenReturn(user);
+            when(userMapper.userToUserDto(user)).thenReturn(userDto);
+
+            // Act
+            UserDto output = userService.createUser(requestUserDto);
+
+            // Assert
+            assertNotNull(output.userId());
+            assertEquals(requestUserDto.userName(), output.userName());
+            assertEquals(requestUserDto.email(), output.email());
+
+        }
+
+        @Test
+        @DisplayName("should return an exception if email already exists")
+        void shouldReturnAnExceptionIfEmailAlreadyExists() {
+            // Arrange
+            RequestUserDto requestUserDto = new RequestUserDto("userName", "email@email.com", "password");
+
+            when(userRepository.existsUserByEmail(requestUserDto.email())).thenReturn(true);
+
+            // Act + Assert
+            assertThrows(EmailAlreadyExistsException.class, () -> userService.createUser(requestUserDto));
+
+            verify(userMapper, never()).requestUserDtoToUser(requestUserDto);
+            verify(userRepository, never()).save(any());
+        }
+    }
+
+
+
+
+
+}
